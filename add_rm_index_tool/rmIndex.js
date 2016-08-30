8a5bc4426697fb49f5853a3e507963a5bb5d756a
malfatti <guimalfatti76@gmail.com>
2016-08-29 22:29:16 -0300

    added promises

--- add_rm_index_tool/rmIndex.js
+++ add_rm_index_tool/rmIndex.js
@@ -3,32 +3,50 @@
  */
 var lineReader = require('line-reader');
 var fs = require('fs');
+var i = 0;
+var Q = require('q');
+var promises = [];
 
 try{
     if(process.argv.length < 4){
-    console.log("please pass two parameters, fist is the input, second is the output");
-     throw new Error('number of parameters are incorrect.')
+        console.log("please pass two parameters, fist is the input, second is the output");
+        throw new Error('number of parameters are incorrect.')
+    }else{
+        var outputFile = process.argv[3]
+        var inputFile = process.argv[2]
     }
 
 
-    fs.writeFile(process.argv[3], '', function (err) {
+    fs.writeFile(outputFile, '.\n', function (err) {
         if (err) throw err;
-        console.log('It\'s saved!');
-    });
-
-    var indexOf = 0;
-    lineReader.eachLine(process.argv[2], function(line, last) {
-        indexOf = line.indexOf(" ");
-        //debugger;
-        fs.appendFile(process.argv[3], line.substr(indexOf).trim() + '\n', function (err) {
+           lineReader.eachLine(inputFile, function(line, last) {        
+            
+            //debugger;
+            promises.push(write_file(line));
+            i++;
+            if(last){
+                Q.allSettled(promises).then(function (results) {
+                    
+                    console.log('Done! ' + i);
+                    return false; // stop reading 
+                    
+                });            
+            }
 
         });
-        if (last) {
-            console.log('Done!');
-            return false; // stop reading
-        }
-
     });
+
+    
+    
 }catch(err){
     console.log(err);
+}
+
+function write_file(line){
+    var deferred = Q.defer();
+    var indexOf = line.indexOf(" ");
+    fs.appendFile(outputFile,line.substr(indexOf).trim() + '\n', function (err) {
+        deferred.resolve();
+    });
+    return deferred.promise;
 }
