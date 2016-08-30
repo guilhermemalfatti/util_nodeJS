88a8a485ea9beed2866917761c9b10fccfeaf83e
malfatti <guimalfatti76@gmail.com>
2016-08-29 21:00:14 -0300

    added try catch

--- add_rm_index_tool/addIndex.js
+++ add_rm_index_tool/addIndex.js
@@ -3,28 +3,31 @@
  */
 var lineReader = require('line-reader');
 var fs = require('fs');
-
-if(process.argv.length < 4){
-	console.log("please pass two parameters, fist is the input, second is the output");
-	 throw new Error('number of parameters are incorrect.')
-}
-
-
-fs.writeFile(process.argv[3], 'Hello Node\n', function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-});
-var i = 0;
-lineReader.eachLine(process.argv[2], function(line, last) {
-    i++;
-    //console.log(i);
-    //debugger;
-    fs.appendFile(process.argv[3], i + ' ' + line + '\n', function (err) {
-
-    });
-    if (last) {
-        console.log('Done!');
-        return false; // stop reading
+try{
+    if(process.argv.length < 4){
+    	console.log("please pass two parameters, fist is the input, second is the output");
+    	 throw new Error('number of parameters are incorrect.')
     }
 
-});
+
+    fs.writeFile(process.argv[3], 'Hello Node\n', function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+    var i = 0;
+    lineReader.eachLine(process.argv[2], function(line, last) {
+        i++;
+        //console.log(i);
+        //debugger;
+        fs.appendFile(process.argv[3], i + ' ' + line + '\n', function (err) {
+
+        });
+        if (last) {
+            console.log('Done!');
+            return false; // stop reading
+        }
+
+    });
+}catch(err){
+    console.log(err);
+}
--- add_rm_index_tool/rmIndex.js
+++ add_rm_index_tool/rmIndex.js
@@ -4,27 +4,31 @@
 var lineReader = require('line-reader');
 var fs = require('fs');
 
-if(process.argv.length < 4){
-	console.log("please pass two parameters, fist is the input, second is the output");
-	 throw new Error('number of parameters are incorrect.')
-}
-
-
-fs.writeFile(process.argv[3], '', function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-});
-
-var indexOf = 0;
-lineReader.eachLine(process.argv[2], function(line, last) {
-    indexOf = line.indexOf(" ");
-    //debugger;
-    fs.appendFile(process.argv[3], line.substr(indexOf).trim() + '\n', function (err) {
-
-    });
-    if (last) {
-        console.log('Done!');
-        return false; // stop reading
+try{
+    if(process.argv.length < 4){
+    console.log("please pass two parameters, fist is the input, second is the output");
+     throw new Error('number of parameters are incorrect.')
     }
 
-});
+
+    fs.writeFile(process.argv[3], '', function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+
+    var indexOf = 0;
+    lineReader.eachLine(process.argv[2], function(line, last) {
+        indexOf = line.indexOf(" ");
+        //debugger;
+        fs.appendFile(process.argv[3], line.substr(indexOf).trim() + '\n', function (err) {
+
+        });
+        if (last) {
+            console.log('Done!');
+            return false; // stop reading
+        }
+
+    });
+}catch(err){
+    console.log(err);
+}
